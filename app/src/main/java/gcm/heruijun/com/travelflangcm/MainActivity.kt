5d91e22ef33bbac2e92cd88b82f35bb379be7b69
何瑞君 <heruijun@heruijundeMacBook-Pro.local>
2017-12-10 17:58:47 +0800

    first commit

---
+++ app/src/main/java/gcm/heruijun/com/travelflangcm/MainActivity.kt
@@ -0,0 +1,12 @@
+package gcm.heruijun.com.travelflangcm
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
